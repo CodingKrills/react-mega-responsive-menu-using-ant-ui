d29c565ee27fefdee0150f345df59d3a149bee9e
codingKrills <pulkitsinghmail@gmail.com>
2021-02-03 19:58:43 +0530

    updates

--- src/Components/Navbar/LeftMenu.js
+++ src/Components/Navbar/LeftMenu.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Menu, Grid } from 'antd';
 const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
 
 const { useBreakpoint } = Grid;
 
@@ -13,27 +12,27 @@ const LeftMenu = () => {
                 <a href='/'>Navigation One</a>
             </Menu.Item>
             <SubMenu key="sub1" title="Navigation Two - Submenu">
-                <MenuItemGroup title="Item 1">
+                <Menu.ItemGroup title="Item 1">
                     <Menu.Item key="setting:1">Option 1</Menu.Item>
                     <Menu.Item key="setting:2">Option 2</Menu.Item>
-                </MenuItemGroup>
-                <MenuItemGroup title="Item 2">
                     <Menu.Item key="setting:3">Option 3</Menu.Item>
                     <Menu.Item key="setting:4">Option 4</Menu.Item>
-                </MenuItemGroup>
+                </Menu.ItemGroup>
             </SubMenu>
             <SubMenu key="SubMenu" title="Navigation Three - Submenu">
                 <Menu.ItemGroup title="Item 1">
                     <Menu.Item key="setting:1">Option 1</Menu.Item>
                     <Menu.Item key="setting:2">Option 2</Menu.Item>
-                </Menu.ItemGroup>
-                <Menu.ItemGroup title="Item 2">
                     <Menu.Item key="setting:3">Option 3</Menu.Item>
                     <Menu.Item key="setting:4">Option 4</Menu.Item>
                 </Menu.ItemGroup>
-                <Menu.ItemGroup title="Item 3">
-                    <Menu.Item key="setting:5">Option 3</Menu.Item>
-                    <Menu.Item key="setting:6">Option 4</Menu.Item>
+                <Menu.ItemGroup title="Item 1">
+                    <Menu.Item key="setting:1">Option 5</Menu.Item>
+                    <Menu.Item key="setting:2">Option 6</Menu.Item>
+                    <Menu.Item key="setting:3">Option 7</Menu.Item>
+                    <Menu.Item key="setting:4">Option 8</Menu.Item>
+                    <Menu.Item key="setting:5">Option 9</Menu.Item>
+                    <Menu.Item key="setting:6">Option 10</Menu.Item>
                 </Menu.ItemGroup>
             </SubMenu>
             <Menu.Item key="2">
